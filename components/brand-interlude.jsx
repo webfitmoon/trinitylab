78c5d7862292f91b265e39a5419a6df024eb952a
webfitmoon <webfitceo@webfit.co.kr>
2026-09-09 09:46:51 +0900

    Add Trinity word-art interlude between sections

---
+++ components/brand-interlude.jsx
@@ -0,0 +1,14 @@
+export default function BrandInterlude() {
+  return (
+    <section className="brand-interlude" aria-label="트리니티에듀랩">
+      <div className="brand-interlude-orb orb-one" aria-hidden="true" />
+      <div className="brand-interlude-orb orb-two" aria-hidden="true" />
+      <div className="brand-interlude-type" aria-hidden="true">
+        <span className="brand-interlude-outline">트리니티에듀랩</span>
+        <span className="brand-interlude-name">트리니티에듀랩</span>
+        <span className="brand-interlude-outline">트리니티에듀랩</span>
+      </div>
+      <p className="brand-interlude-caption">TRINITY EDULAB</p>
+    </section>
+  );
+}
